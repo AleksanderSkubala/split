401f2d00285d62e81f10213312668aa7610d3e33
AleksanderSkubala <olek_s@wtvk.pl>
2019-07-10 22:26:03 +0200

    Making list with overcrowded elements

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ var child;
 var testChild;
 var finalChild;
 var isEnd = false;
+var rest;
+var fragment = dc.createDocumentFragment();
 
 var entry;
 var el;
 
 var pages = dc.getElementsByClassName('page');
-console.log(pages);
 
 if(pages.length > 0) {
     for (let i = 0; i < pages.length; i++) {
         if(pages[i].hasChildNodes() && pages[i].clientHeight > 1123) {
+            rest = pages[i].clientHeight - 1123;
             children = pages[i].childNodes;
 
             for(var entry of children.entries()) { //deleting comments sections from NodeList
@@ -26,36 +28,45 @@ if(pages.length > 0) {
                     el = children[entry[0]-1];
                     el.parentNode.removeChild(el); //deleting NodeText
 
-                    children = pages[0].childNodes;
+                    children = pages[i].childNodes;
                 }
             }
 
             child = children[children.length - 2];
-            console.log(child);
-            switch (child.nodeName) {
-                case "TABLE":
-                    lastList = child.childNodes[1].childNodes;
-                    finalChild = lastList[lastList.length - 2];
-                    break;
+            for (let i2 = 1; i2 < children.length; i2++) {
+                switch (child.nodeName) {
+                    case "TABLE":
+                        lastList = child.childNodes[1].childNodes;
+                        finalChild = lastList[lastList.length - 2];
+                        break;
 
-                case "list":
-                    finalChild = child;
-                    break;
+                    case "list":
+                        finalChild = child;
+                        break;
 
-                default:
-                    lastList = child.childNodes;
-                    while (isEnd === false) {
-                        testChild = lastList[lastList.length -2];
-                        if(testChild.childNodes.length === 1) {
-                            isEnd =  true;
-                            finalChild = testChild;
-                        } else {
-                            lastList = testChild.childNodes;
+                    default:
+                        lastList = child.childNodes;
+                        while (isEnd === false) {
+                            testChild = lastList[lastList.length -2];
+                            if(testChild.childNodes.length === 1) {
+                                isEnd =  true;
+                                finalChild = testChild;
+                            } else {
+                                lastList = testChild.childNodes;
+                            }
                         }
-                    }
-                    break;
+                        break;
+                }
+                fragment.appendChild(finalChild);
+                //appending el to other fragment, if rest is larger than finalChild.clientheight section with switch is repeating
+                if(finalChild.nodeName !== '#text') {
+                    if(rest > finalChild.clientHeight) {
+                        child = children[children.length - 2];
+                        rest -= finalChild.clientHeight;
+                    } else break;
+                }
             }
-            console.log(finalChild);
+            console.log(fragment);
         }
     }
 }
