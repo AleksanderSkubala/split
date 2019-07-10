b54fd4b6072bda7446b2089af0d4c5d14eed96c4
AleksanderSkubala <olek_s@wtvk.pl>
2019-07-10 21:40:42 +0200

    Checking all are they overcrowded pages. Searching in lists.

--- index.js
+++ index.js
@@ -11,46 +11,51 @@ var entry;
 var el;
 
 var pages = dc.getElementsByClassName('page');
+console.log(pages);
 
 if(pages.length > 0) {
-    if(pages[0].hasChildNodes()) {
-        children = pages[0].childNodes;
+    for (let i = 0; i < pages.length; i++) {
+        if(pages[i].hasChildNodes() && pages[i].clientHeight > 1123) {
+            children = pages[i].childNodes;
 
-        for(var entry of children.entries()) { //deleting comments sections from NodeList
-            if(entry[1].nodeName === '#comment') {
-                el = children[entry[0]];
-                el.parentNode.removeChild(el);
+            for(var entry of children.entries()) { //deleting comments sections from NodeList
+                if(entry[1].nodeName === '#comment') {
+                    el = children[entry[0]];
+                    el.parentNode.removeChild(el);
 
-                el = children[entry[0]-1];
-                el.parentNode.removeChild(el); //deleting NodeText
+                    el = children[entry[0]-1];
+                    el.parentNode.removeChild(el); //deleting NodeText
 
-                children = pages[0].childNodes;
-            }
-        }
-
-        child = children[children.length - 2];
-        switch (child.nodeName) {
-            case "TABLE":
-                lastList = child.childNodes[1].childNodes;
-                finalChild = lastList[lastList.length - 2];
-                break;
-
-            case "list":
-                break;
-
-            default:
-                lastList = child.childNodes;
-                while (isEnd === false) {
-                    testChild = lastList[lastList.length -2];
-                    if(testChild.childNodes.length === 1) {
-                        isEnd =  true;
-                        finalChild = testChild;
-                    } else {
-                        lastList = testChild.childNodes;
-                    }
+                    children = pages[0].childNodes;
                 }
-                break;
+            }
+
+            child = children[children.length - 2];
+            console.log(child);
+            switch (child.nodeName) {
+                case "TABLE":
+                    lastList = child.childNodes[1].childNodes;
+                    finalChild = lastList[lastList.length - 2];
+                    break;
+
+                case "list":
+                    finalChild = child;
+                    break;
+
+                default:
+                    lastList = child.childNodes;
+                    while (isEnd === false) {
+                        testChild = lastList[lastList.length -2];
+                        if(testChild.childNodes.length === 1) {
+                            isEnd =  true;
+                            finalChild = testChild;
+                        } else {
+                            lastList = testChild.childNodes;
+                        }
+                    }
+                    break;
+            }
+            console.log(finalChild);
         }
-        console.log(finalChild);
     }
 }
